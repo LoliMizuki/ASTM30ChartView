0d6dff98956a70ff8c5c5a4755e028b306c0e7ff
lolimizuki <lolimizuki@gmail.com>
2016-01-29 12:29:43 +0800

    Apply formal chart color.
    - Fix board line not correct match, draw line by my self.

--- Swift/ASTM30View-Swift/ASColorVectorViewProj/ASTM30RgRfGraphicViewController/ASTM30RgRfGraphicViewController.swift
+++ Swift/ASTM30View-Swift/ASColorVectorViewProj/ASTM30RgRfGraphicViewController/ASTM30RgRfGraphicViewController.swift
@@ -17,16 +17,18 @@ class ASTM30RgRfGraphicViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         coordinateSpace = ASTM30CoordinateSpace(xMin: 50, yMin: 60, xMax: 100, yMax: 140)
-        view.backgroundColor = UIColor(red: 0.502, green: 0.0, blue: 0.251, alpha: 1.0)
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
+        refresh()
+    }
 
+    func refresh() {
         _setAndAddCoordinateViewToView(view)
         _setGrayLayersToView(_coordinateView!)
         _setBoardLinesToView(_coordinateView!)
-        _setCoordinateGridToView(_coordinateView!)
+        _setCoordinateMarksToView(_coordinateView!)
     }
 
 
@@ -34,26 +36,24 @@ class ASTM30RgRfGraphicViewController: UIViewController {
 
     var _coordinateView: UIView? = nil
 
+    deinit {
+        _coordinateView?.removeFromSuperview()
+    }
+}
+
+
+// Graphic components
+extension ASTM30RgRfGraphicViewController {
+
     private func _setAndAddCoordinateViewToView(view: UIView) {
         _coordinateView?.removeFromSuperview()
 
-        let coordinateView = UIView(frame: view.frame*0.9)
-        let offset = CGPoint(
-            x: (view.frame.width - coordinateView.frame.width)/2,
-            y: -(view.frame.height - coordinateView.frame.height)/2
-        )
-
-        coordinateView.backgroundColor = UIColor.blueColor()
+        let coordinateView = UIView(frame: view.frame*0.8)
 
         view.addSubview(coordinateView)
-        coordinateView.center = view.center + offset
+        coordinateView.center = view.center
 
         _coordinateView = coordinateView
-    }
-
-    private func _setBoardLinesToView(view: UIView) {
-        view.layer.borderColor = UIColor.blackColor().CGColor
-        view.layer.borderWidth = 2
     }
 
     private func _setGrayLayersToView(view: UIView) {
@@ -81,21 +81,33 @@ class ASTM30RgRfGraphicViewController: UIViewController {
         let grayLayer = CAShapeLayer()
         grayLayer.path = doubleTriangelPathWithSize(view.frame.size).CGPath
         grayLayer.strokeColor = UIColor.clearColor().CGColor
-        grayLayer.fillColor = UIColor(red: 1.0, green: 1.0, blue: 0.4, alpha: 1.0).CGColor
+        grayLayer.fillColor = UIColor.grayColor().CGColor
         view.layer.addSublayer(grayLayer)
 
-        let deepGrayLayer = CAShapeLayer()
-        deepGrayLayer.path = doubleTriangelPathWithSize(CGSize(width: view.frame.width*0.8, height: view.frame.height)).CGPath
-        deepGrayLayer.strokeColor = UIColor.clearColor().CGColor
-        deepGrayLayer.fillColor = UIColor(red: 0.0, green: 1.0, blue: 1.0, alpha: 1.0).CGColor
-        view.layer.addSublayer(deepGrayLayer)
+        let darkGrayLayer = CAShapeLayer()
+        darkGrayLayer.path = doubleTriangelPathWithSize(CGSize(width: view.frame.width*0.8, height: view.frame.height)).CGPath
+        darkGrayLayer.strokeColor = UIColor.clearColor().CGColor
+        darkGrayLayer.fillColor = UIColor.darkGrayColor().CGColor
+        view.layer.addSublayer(darkGrayLayer)
     }
 
-    private func _setCoordinateGridToView(view: UIView) {
+    private func _setBoardLinesToView(view: UIView) {
+        let layer = CAShapeLayer()
+        layer.frame.size = view.frame.size
+        layer.path = UIBezierPath(rect: CGRect(origin: CGPoint.zero, size: view.frame.size)).CGPath
+
+        layer.lineWidth = 2
+        layer.strokeColor = UIColor.blackColor().CGColor
+        layer.fillColor = UIColor.clearColor().CGColor
+
+        view.layer.addSublayer(layer)
+    }
+
+    private func _setCoordinateMarksToView(view: UIView) {
         let numberOfBlockAtX = 5
         let numberOfBlockAtY = 8
 
-        _addMainGridLinesToView(view,
+        _addMainGridLinesAndMarkTextToView(view,
             numberOfBlockAtX: numberOfBlockAtX,
             numberOfBlockAtY: numberOfBlockAtY
         )
@@ -106,73 +118,56 @@ class ASTM30RgRfGraphicViewController: UIViewController {
         )
     }
 
-    private func _addMainGridLinesToView(view: UIView, numberOfBlockAtX: Int, numberOfBlockAtY: Int) {
-        var labelPositionsAtXAxis = [CGPoint]()
-        var labelPositionsAtYAxis = [CGPoint]()
+    private func _addMainGridLinesAndMarkTextToView(view: UIView, numberOfBlockAtX: Int, numberOfBlockAtY: Int) {
+        var labelPositionsForXAxis = [CGPoint]()
+        var labelPositionsForYAxis = [CGPoint]()
 
         let mainLineslayer = CAShapeLayer()
         mainLineslayer.frame = CGRect(size: view.frame.size)
         mainLineslayer.path = {
             let path = UIBezierPath()
 
-            path.appendPath(_innerLinesPathAtXAxisWithMin(0,
-                max: view.frame.width,
+            path.appendPath(_innerLinesPathAtXAxisFromMin(0,
+                toMax: view.frame.width,
                 numberOfLines: numberOfBlockAtX + 1,
-                yBase: view.frame.height + 10,
-                lengthOfLine: -20,
+                yBase: view.frame.height + 4,
+                lengthOfLine: -16,
                 isIncludeHeadAndTail: true,
-                didAddLine: { from, _, _ in labelPositionsAtXAxis.append(from) }
+                didAddLine: { from, _, _ in labelPositionsForXAxis.append(from) }
             ))
 
-            path.appendPath(_innerLinesPathAtYAxisWithMin(0,
-                max: view.frame.height,
+            path.appendPath(_innerLinesPathAtYAxisFromMin(0,
+                toMax: view.frame.height,
                 numberOfLines: numberOfBlockAtY + 1,
-                xBase: -10,
-                lengthOfLine: 20,
+                xBase: -4,
+                lengthOfLine: 16,
                 isIncludeHeadAndTail: true,
-                didAddLine: { from, _, _ in labelPositionsAtYAxis.append(from) }
+                didAddLine: { from, _, _ in labelPositionsForYAxis.append(from) }
             ))
 
             return path.CGPath
-            }()
+        }()
 
         mainLineslayer.fillColor = UIColor.clearColor().CGColor
-        mainLineslayer.strokeColor = UIColor.greenColor().CGColor
+        mainLineslayer.strokeColor = UIColor.blackColor().CGColor
         mainLineslayer.lineWidth = 2
 
         view.layer.addSublayer(mainLineslayer)
         mainLineslayer.frame.origin = CGPoint.zero
 
-        var maxSize = CGSize.zero
-        var labels = [UILabel]()
+        _addCoordinateMarkLabelsToView(view,
+            textStartValue: coordinateSpace.xMin,
+            positions: labelPositionsForXAxis,
+            offset: CGPoint(x: 0, y: 10)
+        )
 
-        labelPositionsAtXAxis.enumerate().forEach {
-            (index, position) -> () in
-            let offset = CGPoint(x: 0, y: 10)
-            let label = self._addAndGetLabelWithText("\(50 + index*10)", center: position + offset, toView: view)
-
-            maxSize = CGSize(
-                width: max(label.frame.width, maxSize.width),
-                height: max(label.frame.height, maxSize.height)
-            )
-
-            labels.append(label)
-        }
-
-        labelPositionsAtYAxis.reverse().enumerate().forEach {
-            (index, position) -> () in
-            let offset = CGPoint(x: -20, y: 0)
-            let label = self._addAndGetLabelWithText("\(60 + index*10)", center: position + offset, alignment: .Right, toView: view)
-
-            maxSize = CGSize(
-                width: max(label.frame.width, maxSize.width),
-                height: max(label.frame.height, maxSize.height)
-            )
-
-            labels.append(label)
-        }
-
-        labels.forEach { label in label.frame.size = maxSize }
+        _addCoordinateMarkLabelsToView(view,
+            textStartValue: coordinateSpace.yMin,
+            textAlignmen: .Right,
+            positions: labelPositionsForYAxis.reverse(),
+            offset: CGPoint(x: -20, y: 0),
+            useCommonFrameSize: true
+        )
     }
 
     private func _addSubGridLinesToLayer(view: UIView, numberOfBlockAtX: Int, numberOfBlockAtY: Int) {
@@ -183,8 +178,8 @@ class ASTM30RgRfGraphicViewController: UIViewController {
 
             let xInterval = view.frame.width/numberOfBlockAtX.cgFloatValue
             for i in 0..<numberOfBlockAtX {
-                path.appendPath(_innerLinesPathAtXAxisWithMin(xInterval*i.cgFloatValue,
-                    max: xInterval*(i + 1).cgFloatValue,
+                path.appendPath(_innerLinesPathAtXAxisFromMin(xInterval*i.cgFloatValue,
+                    toMax: xInterval*(i + 1).cgFloatValue,
                     numberOfLines: 4,
                     yBase: view.frame.height,
                     lengthOfLine: -8,
@@ -194,8 +189,8 @@ class ASTM30RgRfGraphicViewController: UIViewController {
 
             let yInterval = view.frame.height/numberOfBlockAtY.cgFloatValue
             for i in 0..<numberOfBlockAtY {
-                path.appendPath(_innerLinesPathAtYAxisWithMin(yInterval*i.cgFloatValue,
-                    max: yInterval*(i + 1).cgFloatValue,
+                path.appendPath(_innerLinesPathAtYAxisFromMin(yInterval*i.cgFloatValue,
+                    toMax: yInterval*(i + 1).cgFloatValue,
                     numberOfLines: 4,
                     xBase: 0.0,
                     lengthOfLine: 8,
@@ -206,19 +201,19 @@ class ASTM30RgRfGraphicViewController: UIViewController {
             return path.CGPath
         }()
         subLinesLayer.fillColor = UIColor.clearColor().CGColor
-        subLinesLayer.strokeColor = UIColor.brownColor().CGColor
+        subLinesLayer.strokeColor = UIColor.blackColor().CGColor
         subLinesLayer.lineWidth = 1
         view.layer.addSublayer(subLinesLayer)
         subLinesLayer.frame.origin = CGPoint.zero
     }
 
-    private func _innerLinesPathAtXAxisWithMin(min: CGFloat,
-        max: CGFloat,
+    private func _innerLinesPathAtXAxisFromMin(min: CGFloat,
+        toMax max: CGFloat,
         numberOfLines: Int,
         yBase: CGFloat,
         lengthOfLine: CGFloat,
         isIncludeHeadAndTail: Bool = false,
-        didAddLine: ((from: CGPoint, to: CGPoint, path: UIBezierPath) -> ())? = nil
+        didAddLine: ((pathFrom: CGPoint, pathTo: CGPoint, path: UIBezierPath) -> ())? = nil
     ) -> UIBezierPath  {
         let length = abs(max - min)
         let interval = length/(numberOfLines.cgFloatValue + (isIncludeHeadAndTail ? -1.0 : 1.0))
@@ -236,19 +231,19 @@ class ASTM30RgRfGraphicViewController: UIViewController {
 
             path.appendPath(linePath)
 
-            didAddLine?(from: from, to: to, path: path)
+            didAddLine?(pathFrom: from, pathTo: to, path: path)
         }
 
         return path
     }
 
-    private func _innerLinesPathAtYAxisWithMin(min: CGFloat,
-        max: CGFloat,
+    private func _innerLinesPathAtYAxisFromMin(min: CGFloat,
+        toMax max: CGFloat,
         numberOfLines: Int,
         xBase: CGFloat,
         lengthOfLine: CGFloat,
         isIncludeHeadAndTail: Bool = false,
-        didAddLine: ((from: CGPoint, to: CGPoint, path: UIBezierPath) -> ())? = nil
+        didAddLine: ((pathFrom: CGPoint, pathTo: CGPoint, path: UIBezierPath) -> ())? = nil
     ) -> UIBezierPath  {
         let length = abs(max - min)
         let interval = length/(numberOfLines.cgFloatValue + (isIncludeHeadAndTail ? -1.0 : 1.0))
@@ -266,12 +261,43 @@ class ASTM30RgRfGraphicViewController: UIViewController {
 
             path.appendPath(linePath)
 
-            didAddLine?(from: from, to: to, path: path)
+            didAddLine?(pathFrom: from, pathTo: to, path: path)
         }
 
         return path
     }
 
+    private func _addCoordinateMarkLabelsToView(view: UIView,
+        textStartValue: CGFloat,
+        textAlignmen: NSTextAlignment = .Center,
+        positions: [CGPoint],
+        offset: CGPoint = CGPoint.zero,
+        useCommonFrameSize: Bool = false
+    ) {
+        var maxSize = CGSize.zero
+        var labels = [UILabel]()
+
+        positions.enumerate().forEach {
+            (index, position) -> () in
+            let label = self._addAndGetLabelWithText("\(Int(textStartValue) + index*10)",
+                center: position + offset,
+                alignment: textAlignmen,
+                toView: view
+            )
+
+            maxSize = CGSize(
+                width: max(label.frame.width, maxSize.width),
+                height: max(label.frame.height, maxSize.height)
+            )
+
+            labels.append(label)
+        }
+
+        if useCommonFrameSize {
+            labels.forEach { label in label.frame.size = maxSize }
+        }
+    }
+    
     private func _addAndGetLabelWithText(text: String,
         center: CGPoint,
         alignment: NSTextAlignment = .Center,
@@ -279,9 +305,9 @@ class ASTM30RgRfGraphicViewController: UIViewController {
     ) -> UILabel {
         let label = UILabel()
         label.text = text
+        label.sizeToFit()
 
         view.addSubview(label)
-        label.frame.size = CGSize(width: 30, height: 20.33)
         label.center = center
         label.textAlignment = alignment
 
